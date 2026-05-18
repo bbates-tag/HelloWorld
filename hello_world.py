e25205aac80583186a430eb5706560097201eb2f
Roman Heuser <romansmac@Romans-MacBook-Pro.local>
2026-05-18 11:08:06 -0700

    test

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 def greet(name):
-    return f"Hello, {name}!"
+    print(f"Hello, {name}!")
 
 if __name__ == "__main__":
     name = input("Enter your name: ")
-    message = greet(name)
-    print(message)
+    greet(name)
+    
